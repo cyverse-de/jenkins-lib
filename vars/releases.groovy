deb3ad86bd842923b249169ce97d5b1dba139644
Sarah Roberts <sarahr@cyverse.org>
2019-12-03 15:51:25 -0700

    initial releases library implementation

---
+++ vars/releases.groovy
@@ -0,0 +1,34 @@
+#!/usr/bin/env groovy
+
+@Grab(group='io.github.http-builder-ng', module='http-builder-ng-core', version='1.0.4')
+
+import groovyx.net.http.*
+
+def githubClient(token) {
+    return HttpBuilder.configure {
+        request.uri = 'https://api.github.com'
+        request.accept = ['application/vnd.github.v3+json']
+        request.headers['Authorization'] = "token ${token}"
+    }
+}
+
+def create(token, owner, repo, releaseName) {
+    return githubClient(token).post {
+        request.uri.path = "/repos/${owner}/${repo}/releases"
+        request.contentType = ContentTypes.JSON[0]
+        request.body = [
+            tag_name: releaseName,
+            target_commitish: "master",
+            name: releaseName
+        ]
+    }['id']
+}
+
+def uploadArtifact(token, owner, repo, releaseId, artifactName, artifactContents) {
+    return githubClient(token).post{
+        request.uri = "https://uploads.github.com/repos/${owner}/${repo}/releases/${releaseId}/assets"
+        request.contentType = 'application/octet-stream'
+        request.uri.query = [name: artifactName]
+        request.body = artifactContents
+    }
+}
