85c1fa29b97318c3871cb8a035341fdf7f5a0de5
Sarah Roberts <sarahr@cyverse.org>
2019-12-05 16:13:29 -0700

    Jenkins' readFile DSL method is decoding binary files into a string and mangling the file...trying some alternatives

--- vars/releases.groovy
+++ vars/releases.groovy
@@ -31,6 +31,6 @@ def uploadArtifact(token, owner, repo, releaseId, artifactName, filename) {
     def uri = "https://uploads.github.com/repos/${owner}/${repo}/releases/${releaseId}/assets"
     sh """curl -H 'Authorization: token ${token}' \
                -H 'Content-Type: application/octet-stream' \
-               --data-binary='@${filename}' \
+               --data-binary '@${filename}' \
                '${uri}'"""
 }
